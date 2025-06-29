35398cfc101b980dc1075fb744002f0be418a9fe
Shoaib Bhatti <125224624+Shoaib4447@users.noreply.github.com>
2025-06-29 13:04:42 +0500

    Navbar toggle
    conditionally adding classes to show and hide links from navbar on bars click

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,20 +19,23 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className='links-container show-container'>
-            <ul className='links'>
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className={`${
+            showLinks ? "links-container show-container" : "link-container"
+          }`}
+        >
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <ul className='social-icons'>
           {social.map((socialIcon) => {
             const { id, url } = socialIcon;
